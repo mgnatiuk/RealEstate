f9fe553e3beeacd03db58214c611d9d41de2ac29
mgnatiuk <mikolaj.gnatiuk@gmail.com>
2021-07-28 15:40:33 +0200

    [FEAT] Mapper and models.

---
+++ RealEstate.Domain/Common/PagedResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealEstate.Domain.Common
+{
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; private set; }
+
+        public int TotalPages { get; private set; }
+
+        public int ItemsFrom { get; private set; }
+
+        public int ItemsTo { get; private set; }
+
+        public int TotalItemsCount { get; private set; }
+
+        public PagedResult(List<T> items, int totalCount, int pageSize, int pageNumber)
+        {
+            Items = items;
+            TotalItemsCount = totalCount;
+            ItemsFrom = pageSize * (pageNumber - 1) + 1;
+            ItemsTo = ItemsFrom + pageSize - 1;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+        }
+    }
+}
